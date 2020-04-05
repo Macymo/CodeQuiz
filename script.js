fdd12252c760c82b5881ededaf127fd49faf3e21
Macy <59153195+Macymo@users.noreply.github.com>
2020-04-04 23:36:56 -0400

    updated code

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 //Locating all buttons and timer
 var startButton = document.querySelector("#startQuiz");
-   //console.log("startButton");
-var clearButton = document.querySelector("#clearScores");
-   // console.log("clearButton");
+//console.log("startButton");
 var backButton = document.querySelector("#goBack");
-   // console.log("backButton");
+// console.log("backButton");
+var enter = document.querySelector("enterHighScore");
+var highScoresText = document.querySelector("#initialsText");
+//console.log("text area");
+var clearButton = document.querySelector("#clearScores");
+// console.log("clearButton");
 var clock = document.getElementById("timer");
-   // console.log("clock running");
-var highscoresText = document.querySelector("#initialsText")
-   //console.log("text area");
+// console.log("clock running");
 var mainEl = document.getElementById("main");
 var questionSec = document.createElement("section");
 var questionContainer = document.getElementById("questions");
@@ -17,7 +18,10 @@ var choice1 = document.getElementById("choice1");
 var choice2 = document.getElementById("choice2");
 var choice3 = document.getElementById("choice3");
 var choice4 = document.getElementById("choice4");
-var feedBack = document.getElementById("feedback")
+var feedBack = document.getElementById("feedback");
+var highScores = document.getElementById("highScoresList");
+
+
 
 var myQuestions = [
    {
@@ -45,8 +49,8 @@ var myQuestions = [
 var i = 0;
 var feed = "Wrong!"
 var secondsLeft = 40
-var correct = myQuestions.answer
-var choose = myQuestions.choices
+var answer = myQuestions[i].answer
+
 
 // Start button is clicked to start quiz
 startButton.addEventListener("click", function(){
@@ -87,17 +91,17 @@ function getQuestion() {
    choice4.textContent = fourthChoice.choices[3];
    };
 
-// Event Listeners for when a choice is clicked the next question will appear
+// Event Listeners for when a choice is clicked the next question and/or feedback will appear
    choice1.addEventListener ("click", function (event) {
-      var answer = myQuestions[i].answer
-      if (answer === event.target.textContent) {
-         nextQuestion();
-      } else {
-         showFeedback();
-         nextQuestion();
-      };
-    //  console.log(event.target.textContent);
-    });
+       //  console.log(event.target.textContent);
+       var answer = myQuestions[i].answer
+       if (answer === event.target.textContent) {
+          nextQuestion();
+       } else {
+          showFeedback();
+          nextQuestion();
+       };
+     }); 
     choice2.addEventListener ("click", function (event) {
       var answer = myQuestions[i].answer
       if (answer === event.target.textContent) {
@@ -130,159 +134,81 @@ function getQuestion() {
 function nextQuestion() {
       i++;
       getQuestion();
-   };
-
-// Trying to loop through my nested array choices and my answer to pull the match for the CORRECT ANSWER CLICKED.
-// UNSURE HOW I AM SUPPOSE TO ADD A CLICK EVENT INTO THIS 
-   // var correct = myQuestions.answer 
-   // for (var i = 0; i < correct.length; i++){
-   //    for (var j = 0; j <  choose.length; j++){
-   //       if (myQuestions[i]===choose[j]){
-   //     correctAnswer();
-   //     nextQuestion();
-   //       };
-   //    };
-   // };
-
+};
 
 // When wrong answer is clicked 
 function showFeedback() {
-   feedBack.textContent = feed;
+   feedBack.textContent = feed
     if (feed) {
    secondsLeft -= 10;
-   };
+    };
 };
 
 
-//
-//    var firstChoice = myQuestions[questionIndex];
-//    choice1.textContent = firstChoice.choices[0];
-  
-//    var secondChoice = myQuestions[questionIndex];
-//    choice2.textContent = secondChoice.choices[1];
+//Code for index2.html
 
-//    var thirdChoice = myQuestions[questionIndex];
-//    choice3.textContent = thirdChoice.choices[2];
+var scores = [];
 
-//    var fourthChoice = myQuestions[questionIndex];
-//    choice4.textContent = fourthChoice.choices[3];
+init();
 
-//    choice1.addEventListener ("click", function () {
-//       showFeedback();
-//       nextQuestion();
-//     });
-//     choice2.addEventListener ("click", function () {
-//       nextQuestion();
-//     });
-//     choice3.addEventListener ("click", function () {
-//       showFeedback();
-//       nextQuestion();
-//     });
-//     choice4.addEventListener = ("click", function () {
-//       showFeedback();
-//       nextQuestion();
-//     });
-// };
+function renderScores() {
+  highScores.innerHTML = "";
 
+  // Render a new li for each score
+  for (var i = 0; i < scores.length; i++) {
+    var high = scores[i];
 
-// function nextQuestion() {
-//    var currentQuestion = myQuestions[1];
-//    pulledQuestion.textContent = currentQuestion.question;
+    var li = document.createElement("li");
+    li.textContent = high;
+    li.setAttribute("data-index", i);
 
-//    var firstChoice = myQuestions[1];
-//    choice1.textContent = firstChoice.choices[0];
-  
-//    var secondChoice = myQuestions[1];
-//    choice2.textContent = secondChoice.choices[1];
+    highScores.appendChild(li);
+  };
+};
 
-//    var thirdChoice = myQuestions[1];
-//    choice3.textContent = thirdChoice.choices[2];
+function init() {
+  // Get stored scores from localStorage
+  // Parsing the JSON string to an object
+  var storedHighScores = JSON.parse(localStorage.getItem("scores"));
+  // If scores were retrieved from localStorage, update the scores array to it
+  if (storedHighScores !== null) {
+    scores = storedHighScores;
+  };
+  // Render scores to the DOM
+  renderScores();
+};
 
-//    var fourthChoice = myQuestions[1];
-//    choice4.textContent = fourthChoice.choices[3];
+function storeScores() {
+  // Stringify and set "scores" key in localStorage to scores array
+  localStorage.setItem("scores", JSON.stringify(scores));
+};
 
-//    choice1.addEventListener ("click", function () {
-//       showFeedback();
-//       thirdQuestion();
-//     });
-//     choice2.addEventListener ("click", function () {
-//       showFeedback();
-//       thirdQuestion();
-//     });
-//     choice3.addEventListener ("click", function () {
-//       showFeedback();
-//       thirdQuestion();
-//     });
-//     choice4.addEventListener = ("click", function () {
-      // var one = correctAnswer
-//       thirdQuestion();
-//     });
-// };
+// When form is submitted...
+enter.addEventListener("submit", function(event) {
+  event.preventDefault();
+  var scoresText = highScoresText.value.trim();
+  // Return from function early if submitted scoresText is blank
+  if (scoresText === "") {
+    return;
+  };
+  // Add new scoreText to scores array, clear the input
+  scores.push(scoresText);
+  highScoresText.value = "";
+  // Store updated scores in localStorage, re-render the list
+  storeScores();
+  renderScores();
+});
 
-// function thirdQuestion() {
-//    var currentQuestion = myQuestions[2];
-//    pulledQuestion.textContent = currentQuestion.question;
-
-//    var firstChoice = myQuestions[2];
-//    choice1.textContent = firstChoice.choices[0];
-  
-//    var secondChoice = myQuestions[2];
-//    choice2.textContent = secondChoice.choices[1];
-
-//    var thirdChoice = myQuestions[2];
-//    choice3.textContent = thirdChoice.choices[2];
-
-//    var fourthChoice = myQuestions[2];
-//    choice4.textContent = fourthChoice.choices[3];
-
-//    choice1.addEventListener ("click", function () {
-//       showFeedback();
-//       fourthQuestion();
-//     });
-//     choice2.addEventListener ("click", function () {
-//      var one = correctAnswer
-   //   fourthQuestion();
-//     });
-//     choice3.addEventListener ("click", function () {
-//       showFeedback();
-//       fourthQuestion();
-//     });
-//     choice4.addEventListener = ("click", function () {
-//       showFeedback();
-//       fourthQuestion();
-//     });
-// };
-
-// function fourthQuestion() {
-//    var currentQuestion = myQuestions[3];
-//    pulledQuestion.textContent = currentQuestion.question;
-
-//    var firstChoice = myQuestions[3];
-//    choice1.textContent = firstChoice.choices[0];
-  
-//    var secondChoice = myQuestions[3];
-//    choice2.textContent = secondChoice.choices[1];
-
-//    var thirdChoice = myQuestions[3];
-//    choice3.textContent = thirdChoice.choices[2];
-
-//    var fourthChoice = myQuestions[3];
-//    choice4.textContent = fourthChoice.choices[3];
-
-//    choice1.addEventListener ("click", function () {
-    //   var one = correctAnswer
-//       window.open("index2.html", "_self");
-//     });
-//     choice2.addEventListener ("click", function () {
-//       showFeedback();
-//       window.open("index2.html", "_self");
-//     });
-//     choice3.addEventListener ("click", function () {
-//       showFeedback();
-//       window.open("index2.html", "_self");
-//     });
-//     choice4.addEventListener = ("click", function () {
-//       showFeedback();
-//       window.open("index2.html", "_self");
-//     });
-//    };
+// When the clear button is clicked...
+clearButton.addEventListener("click", function(event) {
+  var element = event.target;
+  // If that element is a button...
+  if (element.matches("click") === true) {
+     // Get its data-index value and clears scores from the list
+   var index = element.parentElement.getAttribute("data-index");
+    scores.splice(index, 1);
+    // Store updated scores in localStorage, re-render the list
+    storeScores();
+    renderScores();
+  };
+});
